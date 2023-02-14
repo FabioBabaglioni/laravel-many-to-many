e82e3877cb0ae819f3d6338097c9c6236365de8a
FabioBabaglioni <fabiobabaglioni@gmail.com>
2023-02-14 14:31:49 +0100

    Creo tabella in db

--- database/migrations/2023_02_14_131946_create_products_table.php
+++ database/migrations/2023_02_14_131946_create_products_table.php
@@ -15,6 +15,13 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+
+            $table -> string('code', 5) -> unique();
+            $table -> string('name', 32);
+            $table -> text('description');
+            $table -> integer('number') -> unsigned();
+            $table -> integer('weight') -> unsigned();
+
             $table->timestamps();
         });
     }
--- database/migrations/2023_02_14_132115_create_categories_table.php
+++ database/migrations/2023_02_14_132115_create_categories_table.php
@@ -14,8 +14,13 @@ return new class extends Migration
     public function up()
     {
         Schema::create('categories', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table -> id();
+
+            $table -> string('code', 5) -> unique();
+            $table -> string('name', 32);
+            $table -> text('description') -> nullable();
+
+            $table -> timestamps();
         });
     }
 
