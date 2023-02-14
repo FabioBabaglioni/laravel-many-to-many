2c954161c7fc21a1d4d45803bbbb0e4bdd55f374
FabioBabaglioni <fabiobabaglioni@gmail.com>
2023-02-14 15:46:16 +0100

    Creo seeder category

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\category;
 use Illuminate\Database\Seeder;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +23,9 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        $this -> call([
+            CategorySeeder :: class
+        ]);
     }
 }
